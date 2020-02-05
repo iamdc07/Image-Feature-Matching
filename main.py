cf7cc2558c3d38e569cb9aea2556527eea937fb8
iamdc07 <chavadadhaval206@gmail.com>
2020-02-05 00:21:15 -0500

    Feature points detected

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import math
 h_window = 5
 
 def load_image():
-    img = cv2.imread("image_sets/graf/img1.ppm", 0)
-    coloured_img = cv2.imread("image_sets/graf/img1.ppm")
+    img = cv2.imread("image_sets/graf/img2.ppm", 0)
+    coloured_img = cv2.imread("image_sets/graf/img2.ppm")
     # img = cv2.imread("image_sets/yosemite/yosemite1.jpg", 0)
     # coloured_img = cv2.imread("image_sets/yosemite/yosemite1.jpg")
     # blurry_merged = np.hstack((graffiti, blur))
@@ -20,20 +20,30 @@ def load_image():
     ix, iy = calculate_gradient(img)
 
     dst = harris_detector(img, coloured_img, height, width, pad, ix, iy)
-    
+
+    feature_points = local_maximum(dst, height, width)
+
+    kp1 = []
+    for x, y, d in feature_points:
+        kp1.append(cv2.KeyPoint(x, y, 1))
+
+    cv2.drawKeypoints(coloured_img, keypoints=kp1, outImage=coloured_img,  color=(0, 0, 255), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_OVER_OUTIMG)
+
+    cv2.imshow('Keypoints ', coloured_img)
+    cv2.waitKey(0)
 
 
 def calculate_gradient(img):
-    sobel_x = np.array([[1, 0, -1],
-                        [2, 0, -2],
-                        [1, 0, -1]])
-
-    sobel_y = np.array([[1, 2, 1],
-                        [0, 0, 0],
-                        [-1, -2, -1]])
-
-    ix = cv2.filter2D(img, -1, sobel_x)
-    iy = cv2.filter2D(img, -1, sobel_y)
+    # sobel_x = np.array([[1, 0, -1],
+    #                     [2, 0, -2],
+    #                     [1, 0, -1]])
+    #
+    # sobel_y = np.array([[1, 2, 1],
+    #                     [0, 0, 0],
+    #                     [-1, -2, -1]])
+    #
+    # ix = cv2.filter2D(img, -1, sobel_x)
+    # iy = cv2.filter2D(img, -1, sobel_y)
 
     ix = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
     iy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
@@ -68,11 +78,11 @@ def harris_detector(img, coloured_img, height, width, pad, ix, iy):
 
             if trace != 0:
                 c = math.floor(det / trace)
-                if c > 10000000:
+                if c > 50000000:
                     dst[y, x] = c
 
 
-    print(img.dtype)
+    # print(dst)
 
     # product_1 = np.multiply(ix2_blurred, iy2_blurred).astype(np.uint8)
     # product_2 = np.multiply(ixiy_blurred, ixiy_blurred).astype(np.uint8)
@@ -124,6 +134,44 @@ def harris_detector(img, coloured_img, height, width, pad, ix, iy):
     # cv2.imshow('', dst)
     # cv2.waitKey()
     return dst
+
+
+def local_maximum(img, height, width):
+    feature_points = []
+
+    temp_img = np.pad(img, (1, 1), 'constant', constant_values=(0, 0))
+
+    pad = math.floor(3 / 2)
+
+    for y in np.arange(pad, height - pad):
+        for x in np.arange(pad, width - pad):
+            roi = temp_img[y - pad:y + pad + 1, x - pad:x + pad + 1]
+
+            max = np.amax(roi)
+            max_location = np.where(max == roi)
+
+            # print(roi)
+            # print(max)
+            # print(max_location)
+
+            max_y = max_location[0]
+            max_x = max_location[1]
+
+            py, px = max_y[0], max_x[0]
+
+            if py == 1 and px == 1:
+                feature_points.append((x - 1, y - 1, temp_img[1, 1]))
+                # print(temp_img[y, x], " is largest in window")
+                # print(max_location[0], " ", max_location[1])
+            else:
+                temp_img[y, x] = 0
+                # print("Point ", temp_img[y, x])
+
+
+    # print(feature_points)
+    feature_points = list(set(feature_points))
+
+    return feature_points
 
 
 if __name__ == '__main__':
